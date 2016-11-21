7ee4ba8dd350fab38cc15ca6c1abe837c59f26a2
Radislav Crechet <radislav.crechet.cr@gmail.com>
2016-11-21 21:55:26 +0200

    Add business logic for display Halsted metrix of classes in selected file and methods in selected class

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -6,16 +6,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+
+import bl.model.IAnalytic;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import bl.metrix.halstead.IHalsteadMetrixCalculator;
@@ -47,6 +48,14 @@ public class Controller implements Initializable {
     Button calculateMetrixButton;
     @FXML
     ListView resultListView;
+    @FXML
+    CheckBox onlyClassCheckBox;
+    @FXML
+    ComboBox <String> onlyClassComboBox;
+    @FXML
+    CheckBox onlyFunctionCheckBox;
+    @FXML
+    ComboBox <String> onlyFunctionComboBox;
 
     private File file;
 
@@ -61,39 +70,117 @@ public class Controller implements Initializable {
             file = fileChooser.showOpenDialog(selectFileButton.getScene().getWindow());
             String fileString = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
             textArea.setText(fileString);
-            calculateMetrixButton.setDisable(false);
+            boolean disabled = (textArea.getText().length() == 0);
+            onlyClassCheckBox.setDisable(disabled);
+            calculateMetrixButton.setDisable(disabled);
+            clearControls();
+            onlyClassComboBox.setDisable(true);
+            onlyClassCheckBox.setSelected(false);
+            resultListView.getItems().clear();
         } catch (IOException e) {
             showAlert(e.getMessage());
             e.printStackTrace();
         }
     }
 
-    public void onClickCalculateMetrix(ActionEvent actionEvent) {
-        IAnalyticFile analyticFile = null;
-        try {
-            analyticFile = new AnalyticFile(file);
-            IHalsteadMetrixCalculator <IAnalyticFile> fileCalculator = new HalsteadMetrixFileCalculator(analyticFile);
-            fileCalculator.calculateMetrix();
+    public void onClickOnlyClassCheckBox(ActionEvent actionEvent) {
+        boolean disabled = !onlyClassCheckBox.isSelected();
+        onlyClassComboBox.setDisable(disabled);
+        if (onlyClassCheckBox.isSelected()) {
+            try {
+                IAnalyticFile analyticFile = new AnalyticFile(file);
+                List <IAnalyticClass> analyticClasses = analyticFile.getClasses();
+                ObservableList <String> items = FXCollections.observableArrayList();
+                for (int index = 0; index < analyticClasses.size(); index++) {
+                    IAnalyticClass analyticClass = analyticClasses.get(index);
+                    items.add(analyticClass.getName());
+                }
+                onlyClassComboBox.setItems(items);
+            } catch (IOException | ParseException e) {
+                showAlert(e.getMessage());
+                e.printStackTrace();
+            }
+        } else {
+            clearControls();
+        }
+    }
 
-            int numberOfUniqueOperators = fileCalculator.getNumberOfUniqueOperators();
+    public void onClickOnlyClassComboBox(ActionEvent actionEvent) {
+        onlyFunctionCheckBox.setDisable(false);
+            try {
+                IAnalyticFile analyticFile = new AnalyticFile(file);
+                List <IAnalyticClass> analyticClasses = analyticFile.getClasses();
+                ObservableList <String> items = FXCollections.observableArrayList();
+                for (int i = 0; i < analyticClasses.size(); i++) {
+                    IAnalyticClass analyticClass = analyticClasses.get(i);
+                    if (analyticClass.getName().equals(onlyClassComboBox.getSelectionModel().getSelectedItem())) {
+                        for (int j = 0; j < analyticClass.getMethods().size(); j++) {
+                            IAnalyticMethod analyticMethod = analyticClass.getMethods().get(j);
+                            items.add(analyticMethod.getName());
+                        }
+                        onlyFunctionComboBox.setItems(items);
+                        break;
+                    }
+                }
+            } catch (IOException | ParseException e) {
+                showAlert(e.getMessage());
+                e.printStackTrace();
+            }
+    }
+
+    public void onClickOnlyFunctionCheckBox(ActionEvent actionEvent) {
+        boolean disabled = !onlyFunctionCheckBox.isSelected();
+        onlyFunctionComboBox.setDisable(disabled);
+        if (disabled) {
+            onlyFunctionComboBox.valueProperty().set(null);
+        }
+    }
+
+    public void onClickCalculateMetrix(ActionEvent actionEvent) {
+        try {
+            IHalsteadMetrixCalculator <?extends IAnalytic> calculator;
+            IAnalyticFile analyticFile = new AnalyticFile(file);
+            calculator = new HalsteadMetrixFileCalculator(analyticFile);
+
+            if (!(!onlyFunctionComboBox.getSelectionModel().isEmpty() && onlyFunctionComboBox.getSelectionModel().getSelectedItem().length() > 0)) {
+                List <IAnalyticClass> analyticClasses = analyticFile.getClasses();
+                Optional<IAnalyticClass> iAnalyticClassOptional = analyticClasses.stream().filter(item->item.getName().equals(onlyClassComboBox.getSelectionModel().getSelectedItem())).findFirst();
+                if (iAnalyticClassOptional.isPresent()){
+                    calculator = new HalsteadMetrixClassCalculator(iAnalyticClassOptional.get());
+                }
+            } else if (!onlyClassComboBox.getSelectionModel().isEmpty() && onlyClassComboBox.getSelectionModel().getSelectedItem().length() > 0) {
+                List <IAnalyticClass> analyticClasses = analyticFile.getClasses();
+                Optional<IAnalyticClass> iAnalyticClassOptional = analyticClasses.stream().filter(item->item.getName().equals(onlyClassComboBox.getSelectionModel().getSelectedItem())).findFirst();
+                if (iAnalyticClassOptional.isPresent()) {
+                    List <IAnalyticMethod> analyticMethods = iAnalyticClassOptional.get().getMethods();
+                    Optional<IAnalyticMethod> iAnalyticMethodOptional = analyticMethods.stream().filter(item->item.getName().equals(onlyFunctionComboBox.getSelectionModel().getSelectedItem())).findFirst();
+                    if (iAnalyticMethodOptional.isPresent()) {
+                        calculator = new HalsteadMetrixMethodCalculator(iAnalyticMethodOptional.get());
+                    }
+                }
+            }
+
+            calculator.calculateMetrix();
+
+            int numberOfUniqueOperators = calculator.getNumberOfUniqueOperators();
             String numberOfUniqueOperatorsString = String.format("Number of Unique Operators: %d", numberOfUniqueOperators);
 
-            int numberOfUniqueOperands = fileCalculator.getNumberOfUniqueOperands();
+            int numberOfUniqueOperands = calculator.getNumberOfUniqueOperands();
             String numberOfUniqueOperandsString = String.format("Number of Unique Operands: %d", numberOfUniqueOperands);
 
-            int numberOfOperators = fileCalculator.getNumberOfOperators();
+            int numberOfOperators = calculator.getNumberOfOperators();
             String numberOfOperatorsString = String.format("Number of Operators: %d", numberOfOperators);
 
-            int numberOfOperands = fileCalculator.getNumberOfOperands();
+            int numberOfOperands = calculator.getNumberOfOperands();
             String numberOfOperandsString = String.format("Number of Operands: %d", numberOfOperands);
 
-            int programVocabulary = fileCalculator.getProgramVocabulary();
+            int programVocabulary = calculator.getProgramVocabulary();
             String programVocabularyString = String.format("Program Vocabulary: %d", programVocabulary);
 
-            int programLength = fileCalculator.getProgramLength();
+            int programLength = calculator.getProgramLength();
             String programLengthString = String.format("Program Length: %d", programLength);
 
-            double programVolume = fileCalculator.getProgramVolume();
+            double programVolume = calculator.getProgramVolume();
             String programVolumeString = String.format("Program Volume: %.2f", programVolume);
 
             ObservableList <String> items = FXCollections.observableArrayList(numberOfUniqueOperatorsString,
@@ -104,13 +191,18 @@ public class Controller implements Initializable {
                     programLengthString,
                     programVolumeString);
             resultListView.setItems(items);
-        } catch (IOException e) {
-            showAlert(e.getMessage());
-            e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (IOException | ParseException e) {
             showAlert(e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private void clearControls() {
+        onlyClassComboBox.getItems().clear();
+        onlyFunctionComboBox.getItems().clear();
+        onlyFunctionComboBox.setDisable(true);
+        onlyFunctionCheckBox.setSelected(false);
+        onlyFunctionCheckBox.setDisable(true);
     }
 
     private void showAlert(String message) {
